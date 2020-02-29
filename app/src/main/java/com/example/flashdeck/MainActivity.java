051c2c6c0f8c0a94d7ad1b31ee30444656a50f67
SajidMA <smahmed@aggies.ncat.edu>
2020-02-29 01:27:13 -0500

    Lab 1: Add Optional Features

--- app/src/main/java/com/example/flashdeck/MainActivity.java
+++ app/src/main/java/com/example/flashdeck/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.flashdeck;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,13 +16,59 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final TextView q1 = (TextView) findViewById(R.id.flashcard_question);
-        final TextView a1 = (TextView) findViewById(R.id.flashcard_answer);
+        final TextView a1 = (TextView) findViewById(R.id.flashcard_answer1);
+        final TextView a2 = (TextView) findViewById(R.id.flashcard_answer2);
+        final TextView a3 = (TextView) findViewById(R.id.flashcard_answer3);
+        final ImageView hide = (ImageView) findViewById(R.id.toggle_choices_hide);
+        final ImageView show = (ImageView) findViewById(R.id.toggle_choices_show);
 
-        q1.setOnClickListener(new View.OnClickListener() {
+
+        a1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                q1.setVisibility(View.INVISIBLE);
+                a1.setBackgroundColor(Color.parseColor("#e74c3c"));
+                a2.setBackgroundColor(Color.parseColor("#f8c471"));
+                a3.setBackgroundColor(Color.parseColor("#229954"));
+            }
+        });
+
+        a2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                a1.setBackgroundColor(Color.parseColor("#f8c471"));
+                a2.setBackgroundColor(Color.parseColor("#e74c3c"));
+                a3.setBackgroundColor(Color.parseColor("#229954"));
+            }
+        });
+
+        a3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                a1.setBackgroundColor(Color.parseColor("#f8c471"));
+                a2.setBackgroundColor(Color.parseColor("#f8c471"));
+                a3.setBackgroundColor(Color.parseColor("#229954"));
+            }
+        });
+
+        hide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                a1.setVisibility(View.INVISIBLE);
+                a2.setVisibility(View.INVISIBLE);
+                a3.setVisibility(View.INVISIBLE);
+                hide.setVisibility(View.INVISIBLE);
+                show.setVisibility(View.VISIBLE);
+            }
+        });
+
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 a1.setVisibility(View.VISIBLE);
+                a2.setVisibility(View.VISIBLE);
+                a3.setVisibility(View.VISIBLE);
+                hide.setVisibility(View.VISIBLE);
+                show.setVisibility(View.INVISIBLE);
             }
         });
     }
