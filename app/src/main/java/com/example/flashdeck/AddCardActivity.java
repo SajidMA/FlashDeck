879553d11e7db25964942a419fda8d6b53f42b73
SajidMA <smahmed@aggies.ncat.edu>
2020-03-06 18:22:47 -0500

    Lab 2: Complete Required and Optional Tasks

---
+++ app/src/main/java/com/example/flashdeck/AddCardActivity.java
@@ -0,0 +1,60 @@
+package com.example.flashdeck;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_card);
+
+        final ImageView close = (ImageView) findViewById(R.id.close_activity);
+        final ImageView save = (ImageView) findViewById(R.id.save_question);
+
+        final String question = getIntent().getStringExtra("question");
+        String answer1 = getIntent().getStringExtra("answer1");
+        String answer2 = getIntent().getStringExtra("answer2");
+        String answer3 = getIntent().getStringExtra("answer3");
+        final EditText q1 = (EditText) findViewById(R.id.new_question);
+        final EditText a1 = (EditText) findViewById(R.id.new_answer1);
+        final EditText a2 = (EditText) findViewById(R.id.new_answer2);
+        final EditText a3 = (EditText) findViewById(R.id.new_answer3);
+        q1.setText(question);
+        a1.setText(answer1);
+        a2.setText(answer2);
+        a3.setText(answer3);
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (((EditText) findViewById(R.id.new_question)).getText().toString().isEmpty() || ((EditText) findViewById(R.id.new_answer1)).getText().toString().isEmpty() || ((EditText) findViewById(R.id.new_answer2)).getText().toString().isEmpty() || ((EditText) findViewById(R.id.new_answer3)).getText().toString().isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(), "Must enter all fields!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Intent data = new Intent();
+                data.putExtra("question", ((EditText) findViewById(R.id.new_question)).getText().toString());
+                data.putExtra("answer1", ((EditText) findViewById(R.id.new_answer1)).getText().toString());
+                data.putExtra("answer2", ((EditText) findViewById(R.id.new_answer2)).getText().toString());
+                data.putExtra("answer3", ((EditText) findViewById(R.id.new_answer3)).getText().toString());
+                setResult(RESULT_OK, data);
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/flashdeck/MainActivity.java
+++ app/src/main/java/com/example/flashdeck/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.flashdeck;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +8,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +24,8 @@ public class MainActivity extends AppCompatActivity {
         final TextView a3 = (TextView) findViewById(R.id.flashcard_answer3);
         final ImageView hide = (ImageView) findViewById(R.id.toggle_choices_hide);
         final ImageView show = (ImageView) findViewById(R.id.toggle_choices_show);
-
+        final ImageView add = (ImageView) findViewById(R.id.add_question);
+        final ImageView edit = (ImageView) findViewById(R.id.edit_question);
 
         a1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,5 +75,47 @@ public class MainActivity extends AppCompatActivity {
                 show.setVisibility(View.INVISIBLE);
             }
         });
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                MainActivity.this.startActivityForResult(intent, 100);
+            }
+        });
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                intent.putExtra("question", ((TextView) findViewById(R.id.flashcard_question)).getText().toString());
+                intent.putExtra("answer1", ((TextView) findViewById(R.id.flashcard_answer1)).getText().toString());
+                intent.putExtra("answer2", ((TextView) findViewById(R.id.flashcard_answer2)).getText().toString());
+                intent.putExtra("answer3", ((TextView) findViewById(R.id.flashcard_answer3)).getText().toString());
+                MainActivity.this.startActivityForResult(intent, 100);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        final TextView q1 = (TextView) findViewById(R.id.flashcard_question);
+        final TextView a1 = (TextView) findViewById(R.id.flashcard_answer1);
+        final TextView a2 = (TextView) findViewById(R.id.flashcard_answer2);
+        final TextView a3 = (TextView) findViewById(R.id.flashcard_answer3);
+
+        if (requestCode == 100 && resultCode == RESULT_OK)
+        { // this 100 needs to match the 100 we used when we called startActivityForResult!
+            String question = data.getExtras().getString("question"); // 'string1' needs to match the key we used when we put the string in the Intent
+            String answer1 = data.getExtras().getString("answer1");
+            String answer2 = data.getExtras().getString("answer2");
+            String answer3 = data.getExtras().getString("answer3");
+            q1.setText(question);
+            a1.setText(answer1);
+            a2.setText(answer2);
+            a3.setText(answer3);
+            Snackbar.make(findViewById(R.id.flashcard_question), "Card successfully created!", Snackbar.LENGTH_SHORT).show();
+        }
     }
 }
