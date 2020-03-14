b7520b00ae696f619ffdcbf22eeec7706d2c9692
SajidMA <smahmed@aggies.ncat.edu>
2020-03-13 23:32:41 -0400

    Lab 3: Complete Requirements - Saving Data

--- app/src/main/java/com/example/flashdeck/MainActivity.java
+++ app/src/main/java/com/example/flashdeck/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.flashdeck;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -10,13 +11,38 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.wajahatkarim3.roomexplorer.RoomExplorer;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    FlashcardDatabase flashcardDatabase;
+    List<Flashcard> allFlashcards;
+    int currentCardDisplayedIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext());
+        allFlashcards = flashcardDatabase.getAllCards();
+        Log.d("Main Activity", String.valueOf(allFlashcards.size()));
+
+        if (allFlashcards != null && allFlashcards.size() > 0) {
+            ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+            ((TextView) findViewById(R.id.flashcard_answer1)).setText(allFlashcards.get(currentCardDisplayedIndex).getWrongAnswer1());
+            ((TextView) findViewById(R.id.flashcard_answer2)).setText(allFlashcards.get(currentCardDisplayedIndex).getWrongAnswer2());
+            ((TextView) findViewById(R.id.flashcard_answer3)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+        }
+
+        else {
+            ((TextView) findViewById(R.id.flashcard_question)).setText("Add a card!");
+            ((TextView) findViewById(R.id.flashcard_answer1)).setText("");
+            ((TextView) findViewById(R.id.flashcard_answer2)).setText("");
+            ((TextView) findViewById(R.id.flashcard_answer3)).setText("");
+        }
 
         final TextView q1 = (TextView) findViewById(R.id.flashcard_question);
         final TextView a1 = (TextView) findViewById(R.id.flashcard_answer1);
@@ -81,6 +107,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
                 MainActivity.this.startActivityForResult(intent, 100);
+                currentCardDisplayedIndex = allFlashcards.size();
             }
         });
 
@@ -92,7 +119,67 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra("answer1", ((TextView) findViewById(R.id.flashcard_answer1)).getText().toString());
                 intent.putExtra("answer2", ((TextView) findViewById(R.id.flashcard_answer2)).getText().toString());
                 intent.putExtra("answer3", ((TextView) findViewById(R.id.flashcard_answer3)).getText().toString());
-                MainActivity.this.startActivityForResult(intent, 100);
+                MainActivity.this.startActivityForResult(intent, 200);
+            }
+        });
+
+        findViewById(R.id.next_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // advance our pointer index so we can show the next card
+                currentCardDisplayedIndex++;
+
+                // make sure we don't get an IndexOutOfBoundsError if we are viewing the last indexed card in our list
+                if (currentCardDisplayedIndex > allFlashcards.size() - 1) {
+                    currentCardDisplayedIndex = 0;
+                }
+
+                // set the question and answer TextViews with data from the database
+                ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                ((TextView) findViewById(R.id.flashcard_answer1)).setText(allFlashcards.get(currentCardDisplayedIndex).getWrongAnswer1());
+                ((TextView) findViewById(R.id.flashcard_answer2)).setText(allFlashcards.get(currentCardDisplayedIndex).getWrongAnswer2());
+                ((TextView) findViewById(R.id.flashcard_answer3)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+
+                a1.setBackgroundColor(Color.parseColor("#f8c471"));
+                a2.setBackgroundColor(Color.parseColor("#f8c471"));
+                a3.setBackgroundColor(Color.parseColor("#f8c471"));
+            }
+        });
+
+        findViewById(R.id.delete_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashcardDatabase.deleteCard(((TextView) findViewById(R.id.flashcard_question)).getText().toString());
+                allFlashcards = flashcardDatabase.getAllCards();
+
+                if (allFlashcards.size() > 0 && allFlashcards != null)
+                {
+                    ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(currentCardDisplayedIndex - 1).getQuestion());
+                    ((TextView) findViewById(R.id.flashcard_answer1)).setText(allFlashcards.get(currentCardDisplayedIndex - 1).getWrongAnswer1());
+                    ((TextView) findViewById(R.id.flashcard_answer2)).setText(allFlashcards.get(currentCardDisplayedIndex - 1).getWrongAnswer2());
+                    ((TextView) findViewById(R.id.flashcard_answer3)).setText(allFlashcards.get(currentCardDisplayedIndex - 1).getAnswer());
+                    a1.setBackgroundColor(Color.parseColor("#f8c471"));
+                    a2.setBackgroundColor(Color.parseColor("#f8c471"));
+                    a3.setBackgroundColor(Color.parseColor("#f8c471"));
+                }
+
+                else
+                {
+                    ((TextView) findViewById(R.id.flashcard_question)).setText("Add a card!");
+                    ((TextView) findViewById(R.id.flashcard_answer1)).setText("");
+                    ((TextView) findViewById(R.id.flashcard_answer2)).setText("");
+                    ((TextView) findViewById(R.id.flashcard_answer3)).setText("");
+                    a1.setBackgroundColor(Color.parseColor("#f8c471"));
+                    a2.setBackgroundColor(Color.parseColor("#f8c471"));
+                    a3.setBackgroundColor(Color.parseColor("#f8c471"));
+                }
+            }
+        });
+
+        findViewById(R.id.show_db).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RoomExplorer.show(MainActivity.this, AppDatabase.class, "flashcard-database");
             }
         });
     }
@@ -115,7 +202,24 @@ public class MainActivity extends AppCompatActivity {
             a1.setText(answer1);
             a2.setText(answer2);
             a3.setText(answer3);
+            flashcardDatabase.insertCard(new Flashcard(question, answer1, answer2, answer3));
+            allFlashcards = flashcardDatabase.getAllCards();
             Snackbar.make(findViewById(R.id.flashcard_question), "Card successfully created!", Snackbar.LENGTH_SHORT).show();
+        }
+
+        if (requestCode == 200 && resultCode == RESULT_OK)
+        { // this 200 needs to match the 200 we used when we called startActivityForResult!
+            String question = data.getExtras().getString("question"); // 'string1' needs to match the key we used when we put the string in the Intent
+            String answer1 = data.getExtras().getString("answer1");
+            String answer2 = data.getExtras().getString("answer2");
+            String answer3 = data.getExtras().getString("answer3");
+            q1.setText(question);
+            a1.setText(answer1);
+            a2.setText(answer2);
+            a3.setText(answer3);
+            flashcardDatabase.updateCard(new Flashcard(question, answer1, answer2, answer3));
+            allFlashcards = flashcardDatabase.getAllCards();
+            Snackbar.make(findViewById(R.id.flashcard_question), "Card successfully edited!", Snackbar.LENGTH_SHORT).show();
         }
     }
 }
